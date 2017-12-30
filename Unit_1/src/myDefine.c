1847db85fcaf87bfe93520d0132e208f7c448eed
lirunrui <77048627@qq.com>
2017-12-30 21:40:54 +0800

    搭建环境

--- Unit_1/src/myDefine.c
+++ Unit_1/src/myDefine.c
@@ -6,3 +6,16 @@
 #define INFEASIBLE -1
 #define OVERFLOW -2
 typedef int Status;
+
+/**
+ *
+ * 基本函数(简单的可以直接用类库来表示，复杂的需要动手写)：
+ * 求最大值：max(表达式1, ...，表达式n)
+ * 求最小值：min(表达式1,...., 表达式n)
+ * 求绝对值：abs(表达式)
+ * 求不足整数值：floor(表达式)
+ * 求进位整数值：ceil(表达式)
+ * 判定文件结束：eof(文件变量) 或eof
+ * 判定行结束：eoln(文件变量) 或eoln
+ */
+
