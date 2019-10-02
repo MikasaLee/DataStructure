1d632476666ec3f36e9ca036f517adbdd6364fbd
lirunrui <77048627@qq.com>
2019-10-02 22:58:55 +0800

    Unit_7

---
+++ Unit_7/src/MGraph.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: MGraph.h
+	> Author: RunRui_Li
+	> Mail: 770486267@qq.com 
+	> Created Time: Wed 02 Oct 2019 10:50:07 PM CST
+ ************************************************************************/
+
+/**
+ * 图的数组(领接矩阵)存储表示
+ */
+#ifndef MGRAPH_H
+#define MGRAPH_H
+
+#define INFINITY INT_MAX	//最大值无穷
+#define MAX_VERTEX_NUM 20	//最大顶点个数
+typedef enum{DG,DN,UDG,UDN} GraphKind;	//有向图，有向网，无向图，无向网
+typedef struct ArcCell{
+
+
+}
+
+
+#endif
